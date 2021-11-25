8f073d414a637231ba11d8f17d23be4b41c5fbd3
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2021-11-25 12:00:54 +0100

    add static js css and initial template

---
+++ static/js/main.js
@@ -0,0 +1,86 @@
+	
+/*
+ *
+ *  Personal Web Page - MarianoSciacco.it
+ *  JS
+ *  
+ *  Developed by Maxel
+ *  Github - this.marianosciacco.it
+ *
+ */
+
+
+	// Convenient display ID function 
+
+    function displayId(id)
+    {
+      if(document.getElementById(id).style.display=="none")
+        document.getElementById(id).style.display="block";
+      else
+        document.getElementById(id).style.display="none";
+    }  
+
+
+    // DOCUMENT READY
+
+	$(document).ready(function() 
+	{
+	 	// Loading splash
+
+	 	$(".name").fadeIn(2000);
+	 	$(".desc").fadeIn(3000);	
+
+
+	 	// To the top button
+
+		 $(window).scroll(function() {
+		    if ($(window).scrollTop() > 300) {
+		      $('#TOP').removeClass('hide');
+		    } else {
+		      $('#TOP').addClass('hide');
+		    }
+		  });
+
+		
+		// Offsets for side icons
+
+		if (screen.width >= 900) {
+		  $('.scroll-1').scrollToFixed({ marginTop: 10, limit: $('.limit-1').offset().top - 220 });
+		  $('.scroll-2').scrollToFixed({ marginTop: 10, limit: $('.limit-2').offset().top - 220 });
+		  $('.scroll-3').scrollToFixed({ marginTop: 10, limit: $('.limit-3').offset().top - 220 });
+		  $('.scroll-4').scrollToFixed({ marginTop: 10, limit: $('.limit-4').offset().top - 220 });
+		  $('.scroll-5').scrollToFixed({ marginTop: 10, limit: $('.limit-5').offset().top - 220 });
+		}
+
+
+		// Add smooth scrolling to all links
+
+		$("a").on('click', function(event) {
+
+
+		    if (this.hash !== "") {
+			    event.preventDefault();
+			    var hash = this.hash;
+
+			    $('html, body').animate({
+			    	scrollTop: $(hash).offset().top
+			    }, 800, function(){
+			    	window.location.hash = hash;
+			    });
+		    } 
+		});
+
+	});
+
+
+			
+	// Title animations
+
+	$("#js-morph").Morphext({
+	    animation: "jackInTheBox",
+	    separator: ",",
+	    speed: 3000,
+	    complete: function () {}
+	});
+
+
